ab0f934e7f8da76ace4ec47ae8262902ea9b899f
e <jannatul0040@gmail.com>
2024-01-14 14:24:45 +0600

    Exploring Dynamic Routes _ Nested Routing

---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DynamicPage = ({ params,searchParams }) => {
+    console.log(searchParams)
+    return (
+        <div>
+            <h1>This is dynamic page:{params.id}</h1>
+        <h2>Search by :{searchParams.category}</h2>
+        </div>
+
+    );
+};
+
+export default DynamicPage;
