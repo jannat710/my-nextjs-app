d4411d90adb8768b67563a77fe502e7e29058c75
e <jannatul0040@gmail.com>
2024-01-14 15:49:39 +0600

    created conter app

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const NewsPage = () => {
+    console.log("Hello World")
     return (
         <div>
         <h1>News Home page</h1>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import Counter from "@/components/Counter/Counter";
+
 
 const HomePage = () => {
+
+  console.log('Hello World')
   return (
     <div>
       <h1>This is next app</h1>
+      <Counter></Counter>
+
     </div>
   );
 };
